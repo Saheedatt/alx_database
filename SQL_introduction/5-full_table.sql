0aee636232d41a950e38ec3d566f72fce1fa264e
Saheedatt <afolabisaheedat1@gmail.com>
2023-08-17 15:09:36 +0100

    Trial 5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,6 @@
 -- Prints the full description of the table first_table
-SHOW CREATE TABLE `first_table`;
+-- SHOW CREATE TABLE `first_table`;
+SELECT TABLE_DEFINITION
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'hbtn_0c_0'
+  AND TABLE_NAME = 'first_table';
