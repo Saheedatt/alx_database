834b25615eaf61264cfde402323baac5ac03c875
Saheedatt <afolabisaheedat1@gmail.com>
2023-08-17 16:42:05 +0100

    SQL- More Queries

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- Create a list of cities found in the database
+USE hbtn_0d_usa;
+
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.state_id = states.id AND states.name = 'California'
+ORDER BY cities.id ASC;
