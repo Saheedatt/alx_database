4056ea8b542d2e2f5039d5b5ed1063beeaaac8e0
Saheedatt <afolabisaheedat1@gmail.com>
2023-08-17 14:34:27 +0100

    SQL INTRODUCTION

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,13 +1,19 @@
+-- Set the table name
+SET @table_name = 'first_table';
+
 -- Get the column information for the specified table
-SELECT
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    CASE WHEN IS_NULLABLE = 'YES' THEN 'DEFAULT NULL' ELSE 'NOT NULL' END AS IS_NULLABLE,
-    COLUMN_DEFAULT,
-    COLUMN_COMMENT
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-AND TABLE_NAME = 'first_table';
+-- Get the database name from the command line argument
+database_name=hbtn_0c_0;
 
+-- Connect to the MySQL server
+mysql -u root -p
 
+-- Select the database
+USE hbtn_0c_0;
+
+-- Get the CREATE TABLE statement for the `first_table` table
+table_name=`SHOW CREATE TABLE first_table`;
+
+-- Print the CREATE TABLE statement
+echo "$create_table_statement" | sed -e 's/CREATE TABLE //'
 
--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,3 +1,7 @@
+--Check if the database hbtn_0c_0 exists
+SHOW DATABASES;
+--Create database
+CREATE DATABASE hbtn_0c_0;
 -- Set the database name and table name
 USE hbtn_0c_0;
 
