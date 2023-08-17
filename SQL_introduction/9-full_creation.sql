e8b3ba3c974936e70c0f89bab333e42c28f436e2
Saheedatt <afolabisaheedat1@gmail.com>
2023-08-17 15:25:44 +0100

    SQL INTRODUCTION

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,13 @@
 -- Create the table second_table if it doesn't exist
-DROP TABLE IF EXISTS second_table;
-CREATE TABLE second_table (
-    id INT,
-    name VARCHAR(256),
-    score INT
-);
-
+-- DROP TABLE IF EXISTS second_table;
+-- CREATE TABLE second_table (
+--   id INT,
+--    name VARCHAR(256),
+--    score INT
+-- );
+CREATE TABLE IF NOT EXISTS second_table(
+    id INT,name VARCHAR(256), score INT
+)
 -- Insert multiple rows into the table second_table
 INSERT INTO second_table (id, name, score) VALUES(1, 'John', 10);
 INSERT INTO second_table (id, name, score) VALUES(2, 'Alex', 3);
