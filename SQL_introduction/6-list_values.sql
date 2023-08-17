b0b8f2a6f5c36138d982b4bfd465270c7e5d15dc
Saheedatt <afolabisaheedat1@gmail.com>
2023-08-17 14:39:40 +0100

    SQL INTRODUCTION

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,7 +1,13 @@
 -- Check if the database hbtn_0c_0 exists
 SHOW DATABASES;
--- Create database
-CREATE DATABASE hbtn_0c_0;
+
+-- Create the table if it doesn't exist
+DROP DATABASE IF EXISTS first_table;
+CREATE TABLE first_table (
+    id INT,                    
+    name VARCHAR(256)          
+);
+
 -- Set the database name and table name
 USE hbtn_0c_0;
 
