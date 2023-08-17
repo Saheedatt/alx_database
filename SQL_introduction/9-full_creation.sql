7025f5ef1e9a515cac36bed90275fa3b88ec7e2b
Saheedatt <afolabisaheedat1@gmail.com>
2023-08-17 15:27:39 +0100

    Final attempt?

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,10 +1,4 @@
 -- Create the table second_table if it doesn't exist
--- DROP TABLE IF EXISTS second_table;
--- CREATE TABLE second_table (
---   id INT,
---    name VARCHAR(256),
---    score INT
--- );
 CREATE TABLE IF NOT EXISTS second_table(
     id INT,name VARCHAR(256), score INT
 )
