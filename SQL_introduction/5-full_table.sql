3ca30de44a8af73aed35657b29069b51dbc49512
Saheedatt <afolabisaheedat1@gmail.com>
2023-08-17 15:16:59 +0100

    SQL INTRODUCTION

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,2 @@
 -- Prints the full description of the table first_table
--- SHOW CREATE TABLE `first_table`;
-SELECT TABLE_DEFINITION
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-  AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE `first_table`;
--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -7,8 +7,8 @@ CREATE TABLE second_table (
 );
 
 -- Insert multiple rows into the table second_table
-INSERT INTO second_table (id, name, score) VALUES
-    (1, 'John', 10),
-    (2, 'Alex', 3),
-    (3, 'Bob', 14),
-    (4, 'George', 8);
+INSERT INTO second_table (id, name, score) VALUES(1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES(2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES(3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
+
