28a795fb1c62f1fc35e2524bca28f8aaa2e4075d
Saheedatt <afolabisaheedat1@gmail.com>
2023-08-17 14:46:02 +0100

    SQL INTRODUCTION

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,15 +1,4 @@
--- Check if the database hbtn_0c_0 exists
-SHOW DATABASES;
 
--- Create the table if it doesn't exist
-DROP DATABASE IF EXISTS first_table;
-CREATE TABLE first_table (
-    id INT,                    
-    name VARCHAR(256)          
-);
 
--- Set the database name and table name
-USE hbtn_0c_0;
-
--- List all rows and fields from the table first_table
+-- This lists all rows and fields from the table first_table
 SELECT * FROM first_table;
