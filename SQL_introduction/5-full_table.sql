751f11b5f00ac8f128be7f395b8b12387bae4a90
Saheedatt <afolabisaheedat1@gmail.com>
2023-08-17 15:02:38 +0100

    SQL INTRODUCTION

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,7 +1,2 @@
--- Set the table name
-SET @table_name = 'first_table';
-
--- Get the column information for the specified table
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = @table_name;
+-- Prints the full description of the table first_table
+SHOW CREATE TABLE `first_table`;
